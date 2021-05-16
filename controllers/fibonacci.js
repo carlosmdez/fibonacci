12b4d0f1358d80ab8160012a90c393da5806c80f
Carlos Méndez <carlos.mdez@outlook.com>
2021-05-16 09:59:40 -0500

    Add validation

--- controllers/fibonacci.js
+++ controllers/fibonacci.js
@@ -1,10 +1,14 @@
 exports.hello = (req, res) => {
-  res.send('Welcome')
+  res.send('Welcome! Add /n at the end of the URL string, where n is an integer.')
 }
 
 exports.fibonacci = (req, res) => {
   const n = req.params.n
   const fibArr = []
+
+  if (isNaN(n) || n < 0) {
+    return res.send('Value not valid, please enter an integers greater or equal than 0.')
+  }
 
   fibArr[0] = 0
   fibArr[1] = 1
@@ -12,6 +16,6 @@ exports.fibonacci = (req, res) => {
     fibArr[i] = fibArr[i - 2] + fibArr[i - 1]
   }
 
-  const response = fibArr[fibArr.length - 1]
+  const response = fibArr[n]
   res.send(`${response}`)
 }
